56be1c5c0e9e0807d393779c99b67393a907960e
Uzair Khan <uzairkhan55465@gmail.com>
2024-03-14 16:43:12 +0500

    Theme Setting

---
+++ src/components/Header/Header.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <div>
+            <h4>Platform Launch</h4>
+            <div>
+                <div>
+                    <button><svg width="12" height="12" xmlns="http://www.w3.org/2000/svg"><path fill="#FFF" d="M7.368 12V7.344H12V4.632H7.368V0H4.656v4.632H0v2.712h4.656V12z" /></svg> Add New Task</button>
+                </div>
+                <div>
+
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Header
